027a4b1e85197320efbd635effa5e35abff550e1
Chen-Hsuan Lin <chlin@cmu.edu>
2019-03-29 17:44:47 -0400

    minor fix

--- glasses/train.sh
+++ glasses/train.sh
@@ -1,4 +1,4 @@
-python3 train_Donly.py --model=D0 --warpN=0 --pertFG=0.2
+python3 train_Donly.py --name=D0 --warpN=0 --pertFG=0.2
 for w in {1..5}; do
 	python3 train_STGAN.py --loadD=0/D0_warp0_it50000 --warpN=$w;
 done
